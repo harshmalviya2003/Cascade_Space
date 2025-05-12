986523f57db510a4cd3d9a65e81d6a4e96483f34
harshmalviya2003 <harshmalviya343@gmail.com>
2025-05-12 20:37:20 +0530

    #udpaet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { Hero } from '@/components/Hero';
 import Mission from '@/components/Mission';
-import Edges from '@/components/Edges';
 import Cards from '@/components/Cards';
 import Benefits from '@/components/Benefits';
 
